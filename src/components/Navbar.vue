1abfc24c8195c29cce1407deff49fac7fa2f4923
DirenCA <direncan_akkaya@hotmail.de>
2024-04-22 00:34:56 +0200

    -add "Navbar.vue" component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<template>
+  <h1>Das ist eine Navbar</h1>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/test">Test</router-link> |
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
