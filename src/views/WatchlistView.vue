178a097afb26219394c91e1028dbc8a6a278b045
Diren <117527330+DirenCA@users.noreply.github.com>
2024-04-30 08:33:47 +0200

    WatchlistView component added

---
+++ src/views/WatchlistView.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Lists'
+}
+</script>
+
+<template>
+  <div class="lists">
+    <h1>This is a page to list things</h1>
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
