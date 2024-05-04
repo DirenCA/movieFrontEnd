c05215927fcc96f948dd77f9131776c167c1053d
DirenCA <direncan_akkaya@hotmail.de>
2024-05-04 13:22:11 +0200

    -configure connection to backend

--- src/views/FilmsView.vue
+++ src/views/FilmsView.vue
@@ -1,5 +1,5 @@
 <script>
-import axios from 'axios'
+import axios from '@/axios.js'
 export default {
   name: 'films',
   data () {
@@ -10,8 +10,10 @@ export default {
   },
   methods: {
     async searchMovies () {
+      console.log('searchMovies called with search term:', this.search) // Log when the method is called
       try {
         const response = await axios.get('/films', { params: { search: this.search } })
+        console.log(response) // Log the response to see what's returned
         this.movies = response.data
       } catch (error) {
         console.error(error)
@@ -24,13 +26,17 @@ export default {
 <template>
   <div class="films">
     <h1>Hier findest du alle Filme!</h1>
-    <form class="form-inline my-2 my-lg-0">  @submit.prevent="searchMovies">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <form @submit.prevent="searchMovies">
+      <input v-model="search" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+    <div>
+      <h1>Gesuchtes Wort: {{ search }}</h1>
+      <ul>
+        <li v-for="movie in movies" :key="movie.id">
+          {{ movie }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
-
-<style scoped>
-
-</style>
