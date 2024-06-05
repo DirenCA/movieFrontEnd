8ecccf412880692bb5289375fa39dd28ecce0d5d
DirenCA <direncan_akkaya@hotmail.de>
2024-06-05 20:50:16 +0200

    -fromatting and added description

--- src/views/FilmsView.vue
+++ src/views/FilmsView.vue
@@ -59,7 +59,7 @@ export default {
             <img class="card-img-top" :src="movie.imageUrl" alt="Movie poster">
             <div class="card-body">
               <h5 class="card-title">{{ movie.title }}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">{{ movie.overview }}</p>
               <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
           </div>
