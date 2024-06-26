f4f3d5f5e31f9ee3b4e1ba5a837d64da6781e038
DirenCA <direncan_akkaya@hotmail.de>
2024-06-26 15:29:07 +0200

    render: axios config change

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'https://moviebe-5i9h.onrender.com:8080'
+  baseURL: 'https://moviebe-5i9h.onrender.com'
 })
 
 export default instance
